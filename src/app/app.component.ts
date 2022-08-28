876f5d5d5f1f709068d77185b86478124d3ad04c
osama <osama.yousry@kites-software.com>
2022-08-28 09:34:30 +0200

    add reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forms-playground';
+  formGroup: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      dob: ['', [Validators.required]],
+    });
+  }
+
+  getErrorMessage(control: AbstractControl | null) {
+    if (control) {
+      if (control.errors) {
+        if (control.errors['required']) {
+          return 'This field is required';
+        }
+        if (control.errors['email']) {
+          return 'This is not a valid email';
+        }
+      }
+    }
+    return '';
+  }
 }
